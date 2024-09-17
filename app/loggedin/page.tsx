7340aa5bf7a8a55777321b5495533f3d1d653869
Mo-dev <sleem88@hotmail.com>
2024-09-17 14:40:56 +0800

    Next Auth With Google Provider

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -2,14 +2,13 @@ import { signInAction } from "@/actions/auth-action";
 import Tasks from "@/components/todovex/tasks";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function LoggedIn() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Login</p>
+      <h1>Todovex</h1>
 
-      <form action={signInAction}>
-        <Button> Login </Button>
-      </form>
+      <p>LoggedIn</p>
+      <Tasks />
     </div>
   );
 }
