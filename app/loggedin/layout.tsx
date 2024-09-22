cf94d5da2fa5858dcd25994ffbdfcb83f3a85ac7
Mo-dev <sleem88@hotmail.com>
2024-09-22 12:45:04 +0800

    ConvexAdapter - Next Auth Configuration with DB

--- app/loggedin/layout.tsx
+++ app/loggedin/layout.tsx
@@ -1,4 +1,3 @@
-import { useSession } from "next-auth/react";
 import Providers from "../providers";
 import { auth } from "@/auth";
 
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,42 @@
 "use client";
 import { ReactNode, useMemo } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { Session } from "next-auth";
 import { SessionProvider, useSession } from "next-auth/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function convexTokenFromSession(session: Session | null): string | null {
+  return session?.convexToken ?? null;
+}
+
+function useAuth() {
+  const { data: session, update } = useSession();
+
+  const convexToken = convexTokenFromSession(session);
+  return useMemo(
+    () => ({
+      isLoading: false,
+      isAuthenticated: session !== null,
+      fetchAccessToken: async ({
+        forceRefreshToken,
+      }: {
+        forceRefreshToken: boolean;
+      }) => {
+        if (forceRefreshToken) {
+          const session = await update();
+
+          return convexTokenFromSession(session);
+        }
+        return convexToken;
+      },
+    }),
+    // We only care about the user changes, and don't want to
+    // bust the memo when we fetch a new token.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(session?.user)]
+  );
+}
 
 export default function Providers({
   children,
@@ -15,7 +47,9 @@ export default function Providers({
 }) {
   return (
     <SessionProvider session={session}>
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+      <ConvexProviderWithAuth client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithAuth>
     </SessionProvider>
   );
 }
