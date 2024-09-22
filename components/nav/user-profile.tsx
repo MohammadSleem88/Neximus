6fe3e6677d136a6d6ef27db8da2957a5606e47c9
Mo-dev <sleem88@hotmail.com>
2024-09-22 20:36:45 +0800

    userprofile

--- components/nav/user-profile.tsx
+++ components/nav/user-profile.tsx
@@ -1,26 +1,55 @@
 "use client";
-import React from "react";
-import Image from "next/image";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { signOutAction } from "@/actions/auth-action";
 
 export default function UserProfile() {
-  const { data: session, status } = useSession();
+  const session = useSession();
 
-  if (status === "loading") {
-    return <p>Loading...</p>; // Display a loading state while session is being fetched
-  }
-
-  if (!session) {
-    return <p>No user found</p>; // Handle case when there's no session
-  }
+  const imageUrl = session?.data?.user?.image;
+  const name = session?.data?.user?.name;
+  const email = session?.data?.user?.email;
 
   return (
-    <Image
-      src={session?.user?.image || "/default-profile.png"} // Provide a fallback image
-      width={24}
-      height={24}
-      alt="User Profile Image"
-      className="rounded-full"
-    />
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="hover:cursor-pointer">
+        <Button
+          variant={"secondary"}
+          className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:px-3 lg:w-full bg-white"
+        >
+          {imageUrl && (
+            <Image
+              src={imageUrl}
+              width={24}
+              height={24}
+              alt={`${name} profile picture`}
+              className="rounded-full"
+            />
+          )}
+          <p className="truncate">{name}</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem className="lg:w-full px-28 flex items-center justify-center">
+          <form action={signOutAction}>
+            <Button
+              type="submit"
+              variant={"ghost"}
+              className="hover:text-primary"
+            >
+              Sign out
+            </Button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
