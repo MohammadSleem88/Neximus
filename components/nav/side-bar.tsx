eb7fe472fcef378dacc8ed983c7ffcc8ac6752ab
Mo-dev <sleem88@hotmail.com>
2024-09-30 01:37:08 +0800

    NavBar Card

--- components/nav/side-bar.tsx
+++ components/nav/side-bar.tsx
@@ -125,14 +125,12 @@ export default function SideBar() {
       <div className="mt-auto p-4">
         <Card x-chunk="dashboard-02-chunk-0">
           <CardHeader className="p-2 pt-0 md:p-4">
-            <CardTitle>Upgrade to Pro</CardTitle>
-            <CardDescription>
-              Unlock all features and get unlimited access to our support team.
-            </CardDescription>
+            <CardTitle>Google Drive</CardTitle>
+            <CardDescription>Upload A File</CardDescription>
           </CardHeader>
           <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
             <Button size="sm" className="w-full">
-              Upgrade
+              Google Drive
             </Button>
           </CardContent>
         </Card>
